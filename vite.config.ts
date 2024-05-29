6ff42ac6b1a6a23653fea130ca99021ceb0e22f1
MarlonChi <marlonchiodelli@hotmail.com>
2024-05-28 22:04:44 -0300

    feat: test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      extension: ["ts"],
+      exclude: [
+        "src/env",
+        "src/lib",
+        "src/http",
+        "src/repositories/prisma",
+        "src/app.ts",
+        "src/server.ts",
+      ],
+    },
+  },
 });
