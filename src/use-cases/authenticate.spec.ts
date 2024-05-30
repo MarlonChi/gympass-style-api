30efa5b6fdbf6a37a4846e496a8e91b80ec9b46a
MarlonChi <marlonchiodelli@hotmail.com>
2024-05-29 22:24:32 -0300

    feat: tests refactor

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,10 +1,17 @@
-import { expect, describe, it } from "vitest";
+import { expect, describe, it, beforeEach } from "vitest";
 import { hash } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { AuthenticateUseCase } from "./authenticate";
 import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 
+let usersRepository: InMemoryUsersRepository;
+let sut: AuthenticateUseCase;
 describe("Authenticate Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(usersRepository);
+  });
+
   it("should be able to authenticate", async () => {
     const usersRepository = new InMemoryUsersRepository();
     const sut = new AuthenticateUseCase(usersRepository);
